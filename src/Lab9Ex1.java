21cdb2fb1cab30e6cf709b6b46dcceabb2de3c23
Monika <mxwierzba@gmail.com>
2025-01-12 21:54:45 +0100

    lab 9 ex 1

--- src/Lab9Ex1.java
+++ src/Lab9Ex1.java
@@ -1,2 +1,18 @@
 public class Lab9Ex1 {
+    public static void main(String[] args) {
+        try{
+            ExceptionGenerator.generateNullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("Found exception: NullPointerException!");
+            e.printStackTrace();
+            System.out.println("Exception description: " + e);
+        }
+    }
+}
+
+class ExceptionGenerator {
+    public static void generateNullPointerException() {
+        String str = null;
+        int length = str.length();
+    }
 }
